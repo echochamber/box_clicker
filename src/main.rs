6dc7ea5be18b0302abcd8266dc37bc7ed6accd18
Jason Schein <jschein@apple.com>
2015-06-25 17:48:43 -0700

    Temp tinkering with window

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 extern crate rand;
+extern crate piston_window;
+
 mod block;
 
+use piston_window::{WindowSettings, PistonWindow, rectangle, clear};
+
+
 fn main() {
-    use rand::Rng;
-
-    let mut rng = rand::thread_rng();
-    let num1 = rng.gen::<u32>();
-    let num2 = rng.gen::<u32>();
-    let num3 = num1 % 7;
-
     let blk = block::ActiveBlock::new();
     println!("{:?}", blk);
+
+    ///////
+
+	let window: PistonWindow = WindowSettings::new("Hello Piston!", [640, 480])
+		.exit_on_esc(true).into();
+
+	for e in window {
+	    e.draw_2d(|c, g| {
+	        clear([1.0; 4], g);
+	        rectangle([1.0, 0.0, 0.0, 1.0], // red
+	                  [0.0, 0.0, 100.0, 100.0],
+	                  c.transform, g);
+	    });
+    }
 }
