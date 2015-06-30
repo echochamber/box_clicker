6efb3de86673aa9a2d883cb7178d66e83b96351e
Jason Schein <jschein@apple.com>
2015-06-29 18:41:01 -0700

    Now works with multiple blocks! Need to fix checking for overlapping blocks though

--- src/block.rs
+++ src/block.rs
@@ -1,3 +1,6 @@
+use std::hash::{Hash, Hasher};
+
+
 #[derive(Debug)]
 pub struct Color {
 	pub r: f32,
@@ -22,77 +25,91 @@ impl Color {
 }
 
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub struct Point {
     pub x: f64,
     pub y: f64
 }
 
-
-#[derive(Debug)]
-pub struct Block {
-	pub color: Color,
-	pub width: f64,
-	pub height: f64
-}
-
-impl Block {
-	pub fn new(color: Color, width: f64, height: f64) -> Block {
-		Block {
-			color: color,
-			width: width,
-			height: height
-		}
+impl Hash for Point {
+	fn hash<H: Hasher>(&self, state: &mut H) {
+		(self.x as u64).hash(state);
+		(self.y as u64).hash(state);
 	}
 }
 
 #[derive(Debug)]
 pub struct ActiveBlock {
 	top_left_corner: Point,
-	block: Block 
+	bottom_right_corner: Point,
+	color: Color
+}
+
+impl Hash for ActiveBlock {
+	fn hash<H: Hasher>(&self, state: &mut H) {
+		self.top_left_corner.hash(state);
+		self.bottom_right_corner.hash(state);
+	}
 }
 
 
 impl ActiveBlock {
-	pub fn new(x_offset: f64, y_offset: f64, size: u32, color: Color) -> ActiveBlock {
-  		let block = Block::new(color, size as f64, size as f64);
+	pub fn new(x_offset: f64, y_offset: f64, size: f64, color: Color) -> ActiveBlock {
 
 		ActiveBlock {
 			top_left_corner: Point{ x: x_offset, y: y_offset },
-			block: block 
+			bottom_right_corner: Point{ x: x_offset + size , y: y_offset + size },
+			color: color
 		}
 	}
 
 	pub fn contains_point(&self, point: &Point) -> bool {
-		return self.top_left_corner.x <= point.x &&
+		return 
+			self.top_left_corner.x <= point.x &&
+			self.bottom_right_corner.x >= point.x &&
 			self.top_left_corner.y <= point.y &&
-			self.top_left_corner.x + self.block.width >= point.x &&
-			self.top_left_corner.y + self.block.height >= point.y
+			self.bottom_right_corner.y >= point.y
 	}
 
-	pub fn move_block(&mut self, point: Point) {
-		self.top_left_corner = point
+	pub fn move_block(&mut self, top_left_corner: Point) {
+		self.bottom_right_corner = Point {
+			x: self.bottom_right_corner.x + (self.top_left_corner.x - top_left_corner.x),
+			y: self.bottom_right_corner.y + (self.top_left_corner.y - top_left_corner.y),
+		};
+
+		self.top_left_corner = top_left_corner;
 	}
 
 	pub fn color(&self) -> [f32; 4] {
-		return self.block.color.as_array()
-	}
-
-	pub fn coordinates_as_array(&self) -> [f64; 4] {
-		return [
-			self.top_left_corner.x,
-			self.top_left_corner.y,
-			self.top_left_corner.x + self.block.width,
-			self.top_left_corner.y + self.block.height
-		];
+		return self.color.as_array()
 	}
 
 	pub fn to_rectangle(&self) -> [f64; 4] {
 		return [
 			self.top_left_corner.x,
 			self.top_left_corner.y,
-			self.block.width,
-			self.block.height
+			self.width(),
+			self.height()
 		];
+	}
+
+	pub fn width(&self) -> f64 {
+		return self.bottom_right_corner.x - self.top_left_corner.x;
+	}
+
+	pub fn height(&self) -> f64 {
+		return self.bottom_right_corner.y - self.top_left_corner.y;
+	}
+
+	pub fn overlaps(&self, other_block: &ActiveBlock) -> bool {
+		if self.top_left_corner.x > other_block.bottom_right_corner.x || other_block.top_left_corner.x > self.bottom_right_corner.x {
+			return false;
+		}
+
+		if self.top_left_corner.y < other_block.bottom_right_corner.y || other_block.top_left_corner.y < self.bottom_right_corner.y {
+			return false
+		}
+
+		return true;
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(hash_default)]
+
 extern crate rand;
 extern crate piston_window;
 extern crate piston;
@@ -7,6 +9,10 @@ mod block;
 use piston_window::{WindowSettings, PistonWindow, rectangle, clear};
 
 use block::{ActiveBlock, Point, Color};
+use std::hash::{hash, Hash, SipHasher};
+
+use std::collections::HashMap;
+
 
 
 fn main() {
@@ -14,13 +20,15 @@ fn main() {
     game.run()
 }
 
+#[derive(Debug, Copy, Clone)]
 struct ViewportDimensions {
     pub w: u32,
     pub h: u32
 }
 
+#[derive(Debug)]
 struct Game {
-    current_block: ActiveBlock,
+    current_blocks: HashMap<u64, ActiveBlock>,
     current_mouse_coordinates: Point,
     viewport_dimensions: ViewportDimensions
 }
@@ -29,14 +37,19 @@ impl Game {
     pub fn new(viewport_w: u32, viewport_h: u32) -> Game {
         let viewport_dimensions = ViewportDimensions { w: viewport_w, h: viewport_h };
 
-        Game {
-            current_block: generate_valid_block_for_viewport(&viewport_dimensions),
+        let mut game = Game {
+            current_blocks: HashMap::new(),
             viewport_dimensions: viewport_dimensions,
             current_mouse_coordinates: Point {
                 x: 0f64,
                 y: 0f64
             }
-        }
+        };
+
+        let first_block = generate_valid_block_for_viewport(&viewport_dimensions);
+        game.current_blocks.insert(hash::<_, SipHasher>(&first_block), first_block);
+
+        game
     }
 
     pub fn run(&mut self) {
@@ -48,8 +61,9 @@ impl Game {
             use piston::input::Input::{Move, Press, Resize};
             use piston::input::Motion::MouseCursor;
             use piston::input::mouse::MouseButton;
+            use piston::input::keyboard::Key;
 
-            self.draw_current_box(&e);
+            self.draw_current_blocks(&e);
 
             match e.event {
                 Some(Event::Input(Move(MouseCursor(x, y)))) => {
@@ -57,14 +71,31 @@ impl Game {
                     self.current_mouse_coordinates.y = y;
                 },
                 Some(Event::Input(Press(Mouse(MouseButton::Left)))) => {
-                    println!("Mouse coordinates: {:?}, Box edges: {:?}", self.current_mouse_coordinates, self.current_block.coordinates_as_array());
-                    if self.current_mouse_coordinates_are_in_block() {
-                        self.new_random_block();
-                        println!("Hit, new block is here {:?}", self.current_block);
+                    println!("Mouse coordinates: {:?}", self.current_mouse_coordinates);
+
+
+                    match self.current_mouse_coordinates_are_in_block() {
+                        Some(key) => {
+                            {
+                                self.current_blocks.remove(&key);
+                            }
+                            {
+                                let new_block = self.new_random_block();
+                                println!("Hit, new block is here {:?}", new_block);
+                                self.current_blocks.insert(hash::<_, SipHasher>(&new_block), new_block);
+                            }
+                        },
+                        None => {}
                     }
                 },
                 Some(Event::Input(Resize(w, h))) => {
                     self.update_viewport_size(w, h)
+                },
+                // N key spawns new boxes
+                Some(Event::Input(Press(Keyboard(Key::N)))) => {
+                    let new_block = self.new_random_block();
+                    println!("Hit, new block is here {:?}", new_block);
+                    self.current_blocks.insert(hash::<_, SipHasher>(&new_block), new_block);
                 },
                 // Print all other types of inputs just for fun
                 Some(Event::Input(input)) => println!("{:?}", input),
@@ -73,8 +104,23 @@ impl Game {
         }
     }
 
-    pub fn new_random_block(&mut self){
-        self.current_block = generate_valid_block_for_viewport(&self.viewport_dimensions)
+    pub fn new_random_block(&self) -> ActiveBlock {
+        let mut done = false;
+        let mut count = 0;
+        let mut block: &ActiveBlock = &generate_valid_block_for_viewport(&self.viewport_dimensions);
+
+        while !done || count > 100 {
+            done = true;
+            for (key, current_block) in self.current_blocks.iter() {
+                if (current_block.overlaps(block)) {
+                    done = false;
+                }
+            }
+
+            let block = generate_valid_block_for_viewport(&self.viewport_dimensions);
+            count = count + 1;
+        }
+        generate_valid_block_for_viewport(&self.viewport_dimensions)
     }
 
     pub fn update_viewport_size(&mut self, w: u32, h: u32) {
@@ -82,19 +128,27 @@ impl Game {
         self.viewport_dimensions.h = h;
     }
 
-    pub fn current_mouse_coordinates_are_in_block(&self) -> bool {
-        return self.current_block.contains_point(&self.current_mouse_coordinates)
+    pub fn current_mouse_coordinates_are_in_block(&self) -> Option<u64> {
+        for (key, current_block) in self.current_blocks.iter() {
+            if current_block.contains_point(&self.current_mouse_coordinates) {
+                return Some(key.clone())
+            }
+        }
+
+        return None;
     }
 
-    pub fn draw_current_box(&self, window: &PistonWindow) {
+    pub fn draw_current_blocks(&self, window: &PistonWindow) {
         window.draw_2d(|c, g| {
             clear([0.8, 0.8, 0.8, 1.0], g);
 
-            rectangle(self.current_block.color(),
-                      self.current_block.to_rectangle(),
+            for (key, current_block) in self.current_blocks.iter() {
+                rectangle(current_block.color(),
+                      current_block.to_rectangle(),
                       c.transform,
                       g
-            );
+                );
+            }
         });
     }
 }
@@ -109,7 +163,7 @@ fn generate_valid_block_for_viewport(viewport_dimensions: &ViewportDimensions) -
     let x_offset = (rng.gen::<u32>() % ((viewport_dimensions.w - size))) as f64;
     let y_offset = (rng.gen::<u32>() % ((viewport_dimensions.h - size))) as f64;
 
-    ActiveBlock::new(x_offset, y_offset, size, generate_random_color())
+    ActiveBlock::new(x_offset, y_offset, size as f64, generate_random_color())
 }
 
 fn generate_random_color() -> Color {
